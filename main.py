378698a1d303458699cfbe723d79a160de681022
rsaez1428 <rafaelsaez@yahoo.com>
2023-12-03 17:59:13 -0300

    New routes.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/home")
+@app.route("/home/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
 app.run(debug=True)
